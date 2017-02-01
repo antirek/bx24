cc55a2519a6c28f0063f6655da1f07a4308c0dd5
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-02-01 17:39:06 +0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,49 @@ var request = require('request');
 
 var APP_CODE = 'local.58918486f13042.31544466';
 var APP_KEY = 'ZT7Z87ojxT0FSl3vFhvZ0oseHZhiazJohTSVrromKbIEhdgE1I';
+var domain = null;
+var auth = null;
 
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/1234', function(req, res, next) {
+
+  var post = {
+    auth: auth,
+    USER_PHONE_INNER: '201',
+    USER_ID: '4',
+    PHONE_NUMBER: '891234',
+    TYPE: '2',
+    CRM_CREATE: '1'
+  };
+
+  var url = domain + 'rest/telephony.externalcall.register.json';
+  
+  var options = {
+    method: 'post',
+    body: post,
+    json: true,
+    url: url
+  }
+  request(options, function (err, res, body) { 
+    console.log(err, res, body);
+  }
+
+
+  res.render('index', { title: 'Express' });
+});
+
+
 router.post('/', function(req, res, next) {
   console.log('params', req.params);
   console.log('req query', req.query);
   console.log('req body', req.body);
 
-  var domain = 'https://' + req.query.DOMAIN;
+  domain = 'https://' + req.query.DOMAIN;
   var url = domain + '/rest/user.current.json?auth=' + req.body.AUTH_ID;
 
   request(url, function (error, response, body) {
