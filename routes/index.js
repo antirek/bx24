2c81f4d3ca036c63cfd6d96901803ddd4de01901
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-02-01 16:33:15 +0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-/* GET home page. */
+
+var APP_CODE = 'local.58918486f13042.31544466';
+var APP_KEY = 'ZT7Z87ojxT0FSl3vFhvZ0oseHZhiazJohTSVrromKbIEhdgE1I';
+
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -11,11 +15,11 @@ router.post('/', function(req, res, next) {
   console.log('params', req.params);
   console.log('req query', req.query);
 
-
   var domain = 'https://' + req.query.DOMAIN;
   var url = domain + '/rest/user.current.json?auth=';
 
   request(url, function (error, response, body) {
+    console.log(response);
     if (!error && response.statusCode == 200) {
       console.log(body);
       res.render('index', { title: 'Express' });
