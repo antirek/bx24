2ef51c073a0bae1ac1e811b96773352cbec331f0
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-02-01 16:09:49 +0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,22 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   console.log('params', req.params);
-  res.render('index', { title: 'Express' });
+  console.log('req query', req.query);
+
+
+  var domain = 'https://' + req.query.DOMAIN;
+  var url = domain + '/rest/user.current.json?auth=';
+
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+      res.render('index', { title: 'Express' });
+    }else {
+      console.log(error);
+    }
+  })
+
+  
 });
 
 module.exports = router;
